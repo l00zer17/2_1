2cf648958ecade36cf7f479f6a64c667a9d5f5e4
l00zer17 <Lukasz.tabor4@gmail.com>
2025-03-19 15:24:20 +0000

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,12 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;  
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
-
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);               
+        System.out.print("Wprowadź tekst: ");
+        String input = scanner.nextLine();     
+        String wynik = input.toLowerCase();               
+        System.out.println("Tekst po zamianie na małe litery: " + wynik);
+        scanner.close();
+    }
 }
